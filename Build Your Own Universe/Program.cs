ed50a885512d00b47bfbc8507fba70a1eb7c6d76
ManleyMan <sweezymac@gmail.com>
2022-04-07 16:23:47 -0700

    intializing project

---
+++ Build Your Own Universe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Build_Your_Own_Universe
+{
+    public class Container
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void Galaxies(string galaxy)
+        {
+
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+        }
+  
+    }
+}
