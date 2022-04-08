0ad75878cb7c5753836d7bd8abf36efa6338f08b
ManleyMan <sweezymac@gmail.com>
2022-04-08 10:50:39 -0700

    adds universe class

---
+++ Build Your Own Universe/Universe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Build_Your_Own_Universe
+{
+    internal class Universe
+    {
+        public string name;
+
+    }
+}
